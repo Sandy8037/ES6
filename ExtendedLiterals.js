be4deaf2a558ece3cd3cf858965c9dd8130ee579
Sandeep Devarapalli <sandy8037@gmail.com>
2017-03-03 12:11:23 -0800

    ES6 Binary & Octal Literals

---
+++ ExtendedLiterals.js
@@ -0,0 +1,12 @@
+// ES6 Binary & Octal Literal
+console.log(0b111110111 === 503);
+console.log(0o767 === 503);
+
+// ES5 Binary & Octal Literal
+// (function () {
+// 'use strict';
+//
+// console.log(parseInt('111110111', 2) === 503);
+// console.log(parseInt('767', 8) === 503);
+//
+// })();
