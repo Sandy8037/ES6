e978b9a0dce529b31054b44d482bae12d45bc7f4
Sandeep Devarapalli <sandy8037@gmail.com>
2017-03-04 12:43:18 -0800

    ES6 Export/Import

---
+++ ExportModule.js
@@ -0,0 +1,7 @@
+// ES6 Import/Export
+import * as math from 'ImportModule';
+console.log(math.sum(math.pi, math.pi));
+
+// ES5 Import/Export
+// var math = LibMath;
+// console.log(math.sum(math.pi, math.pi));
---
+++ ImportModule.js
@@ -0,0 +1,12 @@
+// ES6 Import/Export
+export function sum(x, y) {
+  return x + y;
+}
+export var pi = 3.141593;
+
+// ES5 Import/Export
+// var LibMath = {};
+// LibMath.sum = function (x, y) {
+//   return x + y;
+// };
+// LibMath.pi = 3.141593;
